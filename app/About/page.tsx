35ab1be4eefe6fcfd9f8d01d396cc6cb12a197c0
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-06-18 01:16:10 +0500

    .

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,10 +1,32 @@
+import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 
 export default function About () {
     return (
         <section className="w-full h-screen flex flex-col">
             <HeroSection />
-            
+
+
+
+            <div>
+
+
+
+                <div>
+                    <Image src={"/Full Stack Developer Github.png"} width={800} height={700} alt="About Image" />
+                </div>
+
+
+
+                <div>
+                    
+                </div>
+
+
+            </div>
+
+
+
         </section>
     )
 }
