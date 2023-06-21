1733589465023f3a139b0f7182c7f104247a6ed9
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-06-21 06:53:15 +0500

    .

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,12 +1,24 @@
-import { FC } from "react";
+import Image from "next/image";
+import { FC, ReactNode } from "react";
 
+interface ServiceCardProps {
+  icon: string;
+  title: string;
+  button: JSX.Element;
+}
 
-const Button: FC<CardProps> = ({  }) => {
+const ServiceCard: FC<ServiceCardProps> = ({ icon, title, button }) => {
   return (
-    <button>
-      {children}
-    </button>
+    <div className="flex flex-col justify-center items-center  bg-gradient-to-tl from-[#130428] via-[#38126D] to-[#261045] rounded-xl">
+      <div className="smbil:p-14 mmbil:p-16 lmbil:p-20 laptop-lg:p-24">
+        <div className="relative w-12 h-12">
+          <Image src={icon} fill={true} alt="service icon" />
+        </div>
+        <h3 className="text-white">{title}</h3>
+        {button}
+      </div>
+    </div>
   );
 };
 
-export default Button;
+export default ServiceCard;
