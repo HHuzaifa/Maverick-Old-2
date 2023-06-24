d7e45ee9691e9935609ea77dbcc0faa11cd12c1f
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-06-24 07:50:52 +0500

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image"
+
+
 export default function Footer() {
     return (
-        <footer>
-            
+        <footer className="w-full py-1 bg-black">
+            <div className="flex justify-between items-center mx-auto smbil:max-w-[290px] mmbil:max-w-[340px]  lmbil:max-w-[380px] tab:max-w-[670px] laptop:max-w-[980px] laptop-lg:max-w-[1180px] 4k:max-w-[2000px]">
+            <Image src={"/maverick-logo.svg"} width={60} height={60} alt="footer logo" />
+            <p className="text-white font-linkFont text-xs">© 2023 Maverick. All rights reserved</p>
+            </div>
         </footer>
     )
 }
